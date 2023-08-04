c5519a8e81f16b5b72b6fb4c28b3a14083f5b9a4
jonathanphill <jonathanphill@gmail.com>
2023-08-03 20:51:03 -0400

    basic react components complete now creating the back end of the code

---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,14 @@
+import articles from "./article-content";
+import { Link } from "react-router-dom";
+import ArticlesList from "../components/ArticlesList";
+const ArticlesListPage = () => {
+  return (
+    <>
+      <h1>Articles</h1>
+      <ArticlesList
+      articles={articles}
+      />
+    </>
+  );
+};
+export default ArticlesListPage;
